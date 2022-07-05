775125f92ee073c1ec713c3867a59cfbb49ec45d
dforde <darren.forde@gmail.com>
2022-07-04 19:50:57 -0700

    Merge changes from schema update

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,8 +1,11 @@
 async function getResolvers(
+  getLatestListFn,
+  getDocumentFn,
   getOrderedCatalogFn,
-  latestListFn,
   addItemToCatalogFn,
-  getDocumentFn
+  addItemToCartFn,
+  getListItemsFn,
+  addItemToListFn
 ) {
   const revolvers_impl = {
     Query: {
@@ -10,7 +13,7 @@ async function getResolvers(
         return await getOrderedCatalogFn();
       },
       list: async () => {
-        return await latestListFn();
+        return await getLatestListFn();
       },
     },
     Mutation: {
@@ -39,22 +42,68 @@ async function getResolvers(
           };
         }
       },
+      addItemToCart: async (_, { itemId }) => {
+        try {
+          const result = await addItemToCartFn(itemId);
+          console.log(result);
+
+          return {
+            code: 200,
+            success: true,
+            message: `Item successfully added to cart`,
+            item: { ...result },
+          };
+        } catch (err) {
+          return {
+            code: 403,
+            success: false,
+            message: `Error occurred while adding item to cart`,
+            item: null,
+          };
+        }
+      },
+      addItemToList: async (_, args) => {
+        try {
+          const result = await addItemToListFn(args.input);
+
+          return {
+            code: 200,
+            success: true,
+            message: `Item successfully added to list`,
+            item: result,
+          };
+        } catch (err) {
+          return {
+            code: 403,
+            success: false,
+            message: err.message,
+            item: null,
+          };
+        }
+      },
     },
     List: {
-      items: async ({ items }) => {
-        const itemsToQuery = await items.map((el) =>
-          getDocumentFn("catalog", el.item)
+      items: async ({ id }) => {
+        const items = await getListItemsFn(id);
+
+        const itemsToQuery = items.map((el) =>
+          getDocumentFn("catalog", el.itemId)
         );
 
         const result = await Promise.all(itemsToQuery);
 
-        const mappedResults = items.map(({ item, quantityNeeded, unit }) => {
-          const found = result.find((res) => res.id === item);
-
-          return { item: { ...found }, quantityNeeded, unit };
+        const mappedResults = items.map(({ itemId, ...item }) => {
+          const found = result.find((res) => res.id === itemId);
+          return { catalogId: found.id, ...found, ...item };
         });
 
         return mappedResults;
+      },
+    },
+    AddItemToCartResponse: {
+      item: async ({ item }) => {
+        const catalogItem = await getDocumentFn("catalog", item.itemId);
+        return { catalogId: catalogItem.id, ...catalogItem, ...item };
       },
     },
   };
